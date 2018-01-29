02cb66e292ced38366a4952daf74de0a1dcd3bef
TMA/Nghe Pham <pvnghe@tma.com.vn>
2018-01-29 15:11:24 +0700

    Add hero detail to project

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../heroes/hero';
 
 @Component({
@@ -7,6 +7,7 @@ import { Hero } from '../heroes/hero';
   styleUrls: ['./hero-detail.component.scss']
 })
 export class HeroDetailComponent implements OnInit {
+  @Input() hero: Hero;
 
   constructor() { }
 
