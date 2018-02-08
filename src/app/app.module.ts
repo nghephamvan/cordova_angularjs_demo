4d0d497365d7b89268682951c6ceff87e457e21f
TMA/Nghe Pham <pvnghe@tma.com.vn>
2018-02-08 16:04:40 +0700

    Update adding service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from 'app/hero.service';
+import { MessageService } from './message.service';
+import { MessagesComponent } from './messages/messages.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [MessageService, HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
