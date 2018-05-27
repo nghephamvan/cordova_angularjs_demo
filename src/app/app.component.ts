05ac3abbe020cd626dd99044875881371f12e679
Aki <pvnghe@tma.com.vn>
2018-05-27 14:03:31 +0700

    Update for build platform

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+declare var device;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Tour of Heroes';
+
+  ngOnInit() {
+    document.addEventListener("deviceready", function () {
+      alert('Hello world');
+    }, false);
+  }
 }
